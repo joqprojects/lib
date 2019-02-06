d55e58250cdf15ab3b918912610e9401e3c5f8d3
joq62 <joakim.leche@gmail.com>
2019-02-06 23:12:05 +0100

    created

---
+++ src/lib.erl
@@ -0,0 +1,180 @@
+%%% -------------------------------------------------------------------
+%%% Author  : Joq Erlang
+%%%  
+%%% Created : 10 dec 2012
+%%% -------------------------------------------------------------------
+-module(lib).
+
+-behaviour(gen_server).
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+
+-include("lib/src/lib_local.hrl").
+
+-include("include/tcp.hrl").
+-include("include/dns.hrl").
+-include("include/data.hrl").
+-include("include/dns_data.hrl").
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+
+
+%% --------------------------------------------------------------------
+%% Key Data structures
+%% 
+%% --------------------------------------------------------------------
+
+
+%% --------------------------------------------------------------------
+%% Exported functions
+%% --------------------------------------------------------------------
+
+%% dns functions 
+-export([heart_beat/0
+	]
+      ).
+
+
+-export([start/0,
+	 stop/0
+	]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+%% Gen server functions
+
+start()-> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+stop()-> gen_server:call(?MODULE, {stop},infinity).
+
+
+
+%%-----------------------------------------------------------------------
+
+
+%%-----------------------------------------------------------------------
+
+
+heart_beat()->
+    gen_server:call(?MODULE, {heart_beat},infinity).
+%%-----------------------------------------------------------------------
+
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%
+%% --------------------------------------------------------------------
+init([]) ->  
+    io:format("Service ~p~n",[{?MODULE, 'started '}]),
+    {ok, #state{}}. 
+    
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+
+handle_call({heart_beat}, _From, State) ->
+    DnsInfo=State#state.dns_info,
+    {dns,DnsIp,DnsPort}=State#state.dns_addr,
+    if_dns:cast("dns",latest,{dns,dns_register,[DnsInfo]},{DnsIp,DnsPort}),
+    {reply,ok, State};
+    
+handle_call({stop}, _From, State) ->
+    {stop, normal, shutdown_ok, State};
+
+handle_call(Request, From, State) ->
+     Reply = {unmatched_signal,?MODULE,Request,From},
+    {reply, Reply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast(Msg, State) ->
+    io:format("unmatched match cast ~p~n",[{time(),?MODULE,?LINE,Msg}]),
+    {noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+
+
+handle_info(Info, State) ->
+    io:format("unmatched match cast ~p~n",[{time(),?MODULE,?LINE,Info}]),
+    {noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+%% --------------------------------------------------------------------
+%% Function: 
+%% Description:
+%% Returns: non
+%% --------------------------------------------------------------------
+
+
+%% --------------------------------------------------------------------
+%% Internal functions
+%% --------------------------------------------------------------------
+    
+
+%% --------------------------------------------------------------------
+%% Function: 
+%% Description:
+%% Returns: non
+%% --------------------------------------------------------------------
+local_heart_beat(Interval)->
+  %  io:format(" ~p~n",[{?MODULE,?LINE}]),
+    timer:sleep(Interval),
+    ?MODULE:heart_beat(),
+    spawn(fun()-> local_heart_beat(Interval) end).
+%% --------------------------------------------------------------------
+%% Function: 
+%% Description:
+%% Returns: non
+%% --------------------------------------------------------------------
