4c2052d2a2ecaf70e469a5c0e0ada92e1f8c8c6b
joq62 <joakim.leche@gmail.com>
2019-02-13 23:25:35 +0100

    created log and updated if_dns

---
+++ src/if_log.erl
@@ -0,0 +1,52 @@
+%%% -------------------------------------------------------------------
+%%% Author  : uabjle
+%%% Description : dbase using dets 
+%%%
+%%% Created : 10 dec 2012
+%%% -------------------------------------------------------------------
+-module(if_log).
+
+% 
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+-include("include/dns.hrl").
+-include("include/tcp.hrl").
+-include("include/dns_data.hrl").
+%% --------------------------------------------------------------------
+-record(log,
+	{
+	  service,ip_addr,dns_addr,timestamp,type,severity,msg
+	}).
+
+
+%% External exports
+-compile(export_all).
+
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+%% --------------------------------------------------------------------
+%% Function: 
+%% Description:
+%% Returns: non
+%% --------------------------------------------------------------------
+init(Type,Severity,MsgStr)->
+    rpc:call(node(),if_log,init_log,[Type,Severity,MsgStr]).
+
+init_log(Type,Severity,MsgStr)->
+    {ok,Service}=application:get_application(),
+    {ok,DnsIp}=application:get_env(dns_ip_addr),
+    {ok,DnsPort}=application:get_env(dns_port),
+    {ok,IpAddr}=application:get_env(ip_addr),
+    {ok,Port}=application:get_env(port),
+    Log=#log{service=Service,ip_addr={IpAddr,Port},dns_addr={DnsIp,DnsPort},
+	      timestamp={date(),time()},type=Type,
+	      severity=1,
+	      msg=MsgStr},
+    Log.
